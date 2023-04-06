9bd56374d0eab6114a62e467f35931b4e90d5686
unleashdevops <99628667+unleashdevops@users.noreply.github.com>
2023-04-05 20:43:41 -0400

    Update testPipeline.groovy

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -5,16 +5,16 @@ body.resolveStrategy = Closure.DELEGATE_FIRST
 body.delegate = config
 body()
  node( label: 'linux'){
-        String commit_message = sh(
-            script: "git log -1 --pretty=%B",
-            returnStdout: true
-        )
-        skip_ci=commit_message.contains("[skipci]")
-
-        if(skip_ci){
-            println """
-                [skipci] token FOUND in commit message: $commit_message
-                Skipping pipeline...
+       String commit_message = sh(
+          script: "git log -1 --pretty=%B",
+           returnStdout: true
+       )
+      skip_ci=commit_message.contains("[skipci]")
+     
+     if(skip_ci){
+        println """
+                [skipci] token FOUND in commit message: $commit_message
+                Skipping pipeline...
             """            
             return
         } else{
@@ -28,13 +28,12 @@ body()
  
   if(!skip_ci){
       pipeline {
-
-        agent { label 'linux' }
-        options {
-            buildDiscarder(logRotator(numToKeepStr: '20'))
-            skipDefaultCheckout()
-        }
-        stages {
+       agent { label 'linux' }
+       options {
+           buildDiscarder(logRotator(numToKeepStr: '20'))
+           skipDefaultCheckout()
+       }
+       stages {
             stage ('Clean Workspace') {
                 steps {
                     script {
